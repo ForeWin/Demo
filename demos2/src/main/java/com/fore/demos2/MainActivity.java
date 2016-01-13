a1dc077c5b0d0206b2b4c63e1e46c9926730ab9d
ForeWin <2924227039@qq.com>
2016-01-13 11:15:17 +0800

    Initial commit

---
+++ demos2/src/main/java/com/fore/demos2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.fore.demos2;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
